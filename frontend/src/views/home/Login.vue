6bedaab2160385d89675c4e781622e850faf99d8
gsynf <18813058359@163.com>
2019-10-21 17:12:52 +0800

    修改login.vue

--- frontend/src/views/home/Login.vue
+++ frontend/src/views/home/Login.vue
@@ -1,25 +1,38 @@
 <template>
   <div>
-    <header>
-      <div >
-		    <el-image class="logo" :src="logo_path" alt=""></el-image>
-		    <el-image class="name" :src="name_path" alt=""></el-image>
-    </div>
-    </header>
-    <el-form label-width="100px" class="demo-ruleForm login-container">
-        <h3 class="title">系统登录</h3>
+    <el-row type="flex" justify="center">
+      <el-col :span="12" class="logo">
+		      <el-image :src="logo_path" alt=""></el-image>
+      </el-col>
+      <el-col :span="12" class="name">
+		      <el-image  :src="name_path" alt=""></el-image>
+      </el-col>
+    </el-row>
+    <el-row type="flex">
+      <el-col class="demo-ruleForm login-container">
+        <el-form >
+            <h3 class="title">系统登录</h3>
             <el-link class="loginByMsg" type="primary">短信快捷登录</el-link>
-            <el-form-item  class="username" label="用户名" prop="username">
+            <el-form-item  class="username" prop="username">用户名：
                 <el-input  placeholder="请输入您的用户名"></el-input>
             </el-form-item>
-            <el-form-item  class="password" label="密码" prop="password">
+            <el-form-item  class="password" prop="password">密码：
                 <el-input  placeholder="请输入您的密码" show-password></el-input>
             </el-form-item>
             <el-link href="/register" class="registerNow" type="primary" >没有账户？立即注册</el-link>
             <el-link class="losepwd" type="primary">忘记密码？</el-link>
-            <el-button type="primary"
-            @click.native.prevent="loginSubmit" :loading="logining">登录</el-button>
-    </el-form>
+            <el-form-item>
+              <el-row type="flex" justify="center">
+                <el-col :span="4">
+                  <el-button class="btn" type="primary"
+                    @click.native.prevent="loginSubmit" :loading="logining">登录</el-button>
+                </el-col>
+              </el-row>
+
+            </el-form-item>
+        </el-form>
+      </el-col>
+    </el-row>
   </div>
 </template>
 
@@ -56,20 +69,26 @@ body{
     overflow: hidden;
     position: absolute;
 }
+.el-row {
+  margin-top: 35px;
+  margin-bottom: 80px;
+}
 .logo {
-			/* float: left; */
-      position: relative;
+      /* position: relative;
       left: 250px;
-      top: 30px;
+      top: 30px; */
       width: 15%
 		}
 .name {
-      position: relative;
+      /* position: relative;
       left: 300px;
-      top: 20px;
+      top: 20px; */
       width: 50%
 
 		}
+.el-form {
+  height: 400px;
+}
 .login-container {
     /* 处理圆角，W3C标准 */
     border-radius: 5px;
@@ -79,23 +98,23 @@ body{
     -moz-border-radius: 5px;
     /* 规定背景的绘制区域 */
     background-clip: padding-box;
-    margin: 80px 100px;
     width: 800px;
-    height: 500px;
+    height: 550px;
     padding: 35px 35px 15px 35px;
-    background: rgba(182, 175, 175, 0.3);
-    border: 1px solid #908888;
+    background: rgba(0, 0, 0, .3);
+    /* background: rgba(182, 175, 175, 0.3); */
+    border: 1px solid rgba(0, 0, 0, .3);
     /* 盒子阴影 */
-    box-shadow: 2px 2px 25px #908888;
+    box-shadow: 2px 2px 25px #000;
+    /* 子绝父相 */
+    position: relative;
+    left: 80px;
+    top: 35px;
 }
 .login-container .title {
     margin: 0px auto 40px auto;
     text-align: center;
-    color: #505458;
-}
-.el-form {
-    /* 子绝父相 */
-    position: relative;
+    color: #FF9B1F;
 }
 .loginByMsg,
 .losepwd {
@@ -110,11 +129,11 @@ body{
 
 }
 .username,
-.passwoed {
-    margin: 80px 0px 80px 0px;
+.password {
+  margin: 80px 0px 80px 0px;
+  color: #FF9B1F
 }
-.el-button {
-    margin: 100px 350px;
-}
-
+/* .btn {
+  margin: 50px 220px;
+} */
 </style>
